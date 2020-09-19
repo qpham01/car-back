8ca4a1dc863a85a27a77c206de7142ca0083ac2b
= <quoc_pham@msn.com>
2020-09-18 21:27:09 -0500

    Switch to validate.

--- src/main/java/com/qpham/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/qpham/cardatabase/CardatabaseApplication.java
@@ -34,29 +34,29 @@ public class CardatabaseApplication {
         logger.info("Hello Spring Boot");
     }
 
-    @Bean
-    CommandLineRunner runner() {        
-        return args -> {
-            // Add owner objects and save these to db
-            Owner owner1 = new Owner("John", "Johnson");
-            Owner owner2 = new Owner("Mary", "Robinson");
-            orepository.save(owner1);
-            orepository.save(owner2);
-
-            // Add car object with link to owners and save these to db.
-            Car car = new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000);
-            repository.save(car);
-            car = new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000);
-            repository.save(car);
-            car = new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000);
-            repository.save(car);
-            
-            urepository.save(new User("user",
-           "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi",
-            "USER"));
-            urepository.save(new User("admin",
-            "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", 
-            "ADMIN"));
-        };
-    }
+//    @Bean
+//    CommandLineRunner runner() {        
+//        return args -> {
+//            // Add owner objects and save these to db
+//            Owner owner1 = new Owner("John", "Johnson");
+//            Owner owner2 = new Owner("Mary", "Robinson");
+//            orepository.save(owner1);
+//            orepository.save(owner2);
+//
+//            // Add car object with link to owners and save these to db.
+//            Car car = new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000);
+//            repository.save(car);
+//            car = new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000);
+//            repository.save(car);
+//            car = new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000);
+//            repository.save(car);
+//            
+//            urepository.save(new User("user",
+//           "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi",
+//            "USER"));
+//            urepository.save(new User("admin",
+//            "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", 
+//            "ADMIN"));
+//        };
+//    }
 }
